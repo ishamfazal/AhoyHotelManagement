c9d5c72764d7fce67086951aad6b3bf730bdf13f
ishamfazal <ishamfazal@gmail.com>
2022-08-12 17:33:42 +0400

    Initial Commit for Hotel Management API

---
+++ AhoyHotel/Ahoy.Hotel.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Ahoy.Hotel.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
